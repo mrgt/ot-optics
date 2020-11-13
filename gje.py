0f56373a80503dbf4b9e24aad615f62f352f5bca
AnatGllt <anatole@gallouet.fr>
2020-11-13 17:40:09 +0100

    gje.py contient les functions pour resoudre le pb Near Field parallel reflector, test_gje.py est le fichier qui contient les tests.

---
+++ gje.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from geometry import *
+from ot_metasurfaces import *
+import numpy as np
+import pypower
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+
+
+
+
+def mob_to_pow(y, psi):
+    """Return the 3d point c and distance r of the 3d 
+    power diagram associated to the 2d mobius diagram of center y and distance k"""
+    y1 = y[0]
+    y2 = y[1]
+    c = np.array([psi * y1 / 2, psi * y2 / 2, -psi / 4])
+    r = psi**2 / 16 + 1 / (2 * psi) + (psi**2 / 4 - psi / 2)*(y1**2 + y2**2)
+    return [c,r]
+
+def G(x,y,v):
+    """Generating function for NF-par"""
+    return 1 / (2*v) - (v/2)*np.linalg.norm(x - y)**2
+
+def make_mobius(Y, psi, k = 20):
+    """return the Mobius diagram associated to the cost G defined above"""
+    quad = Quadric(np.array([-1,-1,0]), np.array([0,0,1]), 0)
+    N = len(Y)
+    X = np.zeros((N,3))
+    w = np.zeros(N)
+    for i in range(N):
+        v = mob_to_pow(Y[i], psi[i])
+        X[i] = v[0]
+        w[i] = v[1]
+    cells = make_power(X,w)
+    curves = []
+    for i in range(N):
+        c = intersect_cell_with_quadric(X,cells[i],quad,k)
+        curves.append(c)
+    return curves
+
+
+
+def g(x,y_i,y_j,psi_i, psi_j):
+    """Function to integrate for computation of DH_i/Dpsi_j of NF-par problem"""
+    return (1 / (2 * psi_j**2) + np.linalg.norm((x - y_j)**2/2) )/np.linalg.norm(psi_i * y_i - psi_j * y_j + (psi_j - psi_i)*x)
+
+
+def test_twist(Y, psi):
+    """ Test the twist assumption on psi for NF-parallel reflector, whice is psi_i < gamma = inf 1 / ||x - y|| """
+    x = np.array([[1,1], [-1,1],[-1,-1], [1, -1]])
+    N = len(Y)
+    Y1 = np.copy(Y)
+    Y2 = np.copy(Y)
+    Y3 = np.copy(Y)
+    Y4 = np.copy(Y)
+    for i in range(N):
+        Y1[i] = Y1[i] - x[0]
+        Y2[i] = Y2[i] - x[1]
+        Y3[i] = Y3[i] - x[2]
+        Y4[i] = Y4[i] - x[3]
+    m = 1e-15
+    for i in range(N):
+        ma = max(np.linalg.norm(Y1[i]), np.linalg.norm(Y2[i]), np.linalg.norm(Y3[i]), np.linalg.norm(Y4[i]))
+        if ma > m:
+            m = ma
+    m = 1/m
+    for i in range(N):
+        if psi[i] > m:
+            return False
+    return True
+
+def compute_H_DH_from_mobius(Y, psi, allcurves):
+    """Computes H and DH from a mobius diagram stored in allcurves"""
+    N = len(allcurves)
+    areas = np.zeros(N)
+    DH = np.zeros((N,N))
+    for i in range(N):
+        curves = allcurves[i]
+        for j,curvesj in curves.items():
+            for crv in curvesj:
+                crv = np.array(crv)
+                for k in range(crv.shape[0]-1):
+                    a = crv[k,0:2]
+                    b = crv[k+1,0:2]
+                    areas[i] += np.linalg.det(np.array([a,b]))/2
+                    if j<0:
+                        continue
+                    length  = np.linalg.norm(b-a)
+                    #x = (a + b) / 2
+                    #hij = length * g(x, Y[i], Y[j], psi[i], psi[j])
+                    hij = length * (g(a, Y[i], Y[j], psi[i], psi[j]) + g(b, Y[i], Y[j], psi[i], psi[j]))/2
+                    DH[i,j] += hij
+                    DH[i,i] -= hij
+    return areas, DH
+
+def compute_H_DH(Y, psi):
+    return compute_H_DH_from_mobius(Y, psi, make_mobius(Y, psi))
+
+def solve_gje(Y, psi, stoperr=1e-8, threshold = 1e-12):
+    N = len(psi)
+    nu = (4/N) * np.ones(N)
+    h,dh = compute_H_DH(Y, psi)
+    err = np.linalg.norm(h - nu)
+    it = 0
+    while err > stoperr:
+        t = 1
+        print("h = {}".format(h))
+        print("it={}, error={}, tau={}".format(it,err,t))
+        it += 1
+        dh[0,0] += 1 #Astuce detaillée dans la preuve de convergence
+        u = np.linalg.solve(dh, h - nu)
+        psi -=  u     
+        h,dh = compute_H_DH(Y, psi)
+        while min(h) < threshold:
+            t /= 2
+            psi += t * u
+            h,dh = compute_H_DH(Y, psi)
+        err = np.linalg.norm(h - nu)
+    return psi
+
---
+++ test_gje.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from geometry import *
+from ot_metasurfaces import *
+from gje import *
+import numpy as np
+import pypower
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+
+
+def check_gradient(f,gradf,x0):
+    N = len(x0)
+    gg = np.zeros((N,N))
+    for i in range(N):
+        eps = 1e-6
+        e = np.zeros(N)
+        e[i] = eps
+        gg[i,:] = (f(x0+e) - f(x0-e))/(2*eps)
+    print('erreur numérique dans le calcul du gradient: %g (doit être petit)' % np.linalg.norm(gradf(x0)-gg))
+    print(gradf(x0))
+    print(gg)
+
+
+
+#---------------- bloc de test calcul de gradient-----------------------------
+
+N = 5
+Y = np.array([[ 0.62074898, -0.00207409],
+[ 0.9568784 , -0.06380766],
+[ 0.69871951,  0.87968212],
+[-0.87548466, -0.91384685],
+[-0.41198645, -0.76459244]])
+
+H = lambda psi: compute_H_DH_from_mobius(Y, psi, make_mobius(Y, psi, k=100))[0]
+DH = lambda psi: compute_H_DH_from_mobius(Y, psi, make_mobius(Y, psi, k=100))[1]
+
+psi = 0.1 *(np.ones(N) + 0.01 * np.array([0.1, 0.6, -0.1, 1.7, -0.6]))
+print("twist = ",test_twist(Y, psi))
+np.set_printoptions(2)
+check_gradient(H,DH,psi)
+
+M = make_mobius(Y, psi, k=100)
+
+h,dh = compute_H_DH_from_mobius(Y, psi, M)
+dh2 = dh.copy()
+dh2[0,0] += 1
+print("H = ",h)
+print("DH = ", dh)
+print("det(DH) =", np.linalg.det(dh), "det(DH + e_1,1) =", np.linalg.det(dh2))
+#--------------------------------------------------------------------------------
+
+
+
+
+# N = 5
+# Y = np.random.rand(N,2) 
+# test_twist(Y, np.ones(N))
+# psi = solve_gje(Y, np.ones(N))
+# plot_laguerre(M)
+
+# plt.show()
